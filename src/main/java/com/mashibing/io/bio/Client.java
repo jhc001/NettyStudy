f1f95d68a036add9235c73c0a9e3eaae2f4c35b8
jhc <964706001@qq.com>
2020-02-08 19:32:23 +0800

    test

--- src/main/java/com/mashibing/io/bio/Client.java
+++ src/main/java/com/mashibing/io/bio/Client.java
@@ -5,15 +5,38 @@ import java.io.OutputStream;
 import java.net.Socket;
 
 public class Client {
-    public static void main(String[] args) throws IOException {
-        Socket s = new Socket("127.0.0.1", 8888);
-        s.getOutputStream().write("HelloServer".getBytes());
-        s.getOutputStream().flush();
-        //s.getOutputStream().close();
-        System.out.println("write over, waiting for msg back...");
-        byte[] bytes = new byte[1024];
-        int len = s.getInputStream().read(bytes);
-        System.out.println(new String(bytes, 0, len));
-        s.close();
-    }
+	
+	private OutputStream out;
+	
+	private String name;
+	
+	private void init() throws Exception {
+		Socket s = new Socket("127.0.0.1", 8888);
+		this.out = s.getOutputStream();
+	}
+	
+	public void sayName() throws IOException {
+		this.out.write(name.getBytes());
+	}
+
+	public Client(String name) throws Exception {
+		super();
+		this.name = name;
+		init();
+	}
+	
+	public static void main(String[] args) throws Exception {
+		
+		Client c1 = new Client("c1");
+		Client c2 = new Client("c2");
+		
+		c1.sayName();
+		c2.sayName();
+		c1.sayName();
+		c2.sayName();
+		
+	}
+	
+    
+    
 }
